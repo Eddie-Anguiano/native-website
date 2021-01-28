e0c694c883a2d7189a447e80070c392b9aadc82c
Eddie Anguiano <eddiesanguiano@gmail.com>
2021-01-27 20:30:29 -0800

    add parallax effect to Hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,16 +1,32 @@
+import { useState, useEffect } from "react";
 import styles from "../styles/components/Hero.module.scss";
 import Wrapper from "./Wrapper";
 
 export default function Hero() {
+  const [offsetY, setOffset] = useState(0);
+  const handleScroll = () => setOffset(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className={styles.Hero}>
+      <div
+        className={styles.test}
+        style={{ transform: `translateY(${offsetY * 0.5}px)` }}></div>
       <img
         src="/images/background-part.png"
         alt=""
         className={styles.parallax}
       />
       <Wrapper>
-        <div className={styles.container}>
+        <div
+          className={styles.container}
+          style={{ transform: `translateY(${offsetY * 0.5}px)` }}>
           <h1 className={styles.header}>
             Puvugna Wetland
             <br /> Protectors
