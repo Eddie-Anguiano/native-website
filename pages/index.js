e07775c55003739125053852fe6072640a57a255
Eddie Anguiano <eddiesanguiano@gmail.com>
2021-02-02 14:07:13 -0800

    fixed file naming error in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
-import styles from "../styles/pages/home.module.scss";
+import styles from "../styles/pages/Home.module.scss";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import IconSection from "../components/IconSection";
